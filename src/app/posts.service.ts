d959358c929a0682784dd5b0df713c710631daf0
Austin Campbell <austin.campbell@fiserv.com>
2022-02-05 17:46:01 -0500

    updates

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Post } from './post.model';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
+import { Subject, throwError} from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
+  error = new Subject<string>();
 
   constructor(private http: HttpClient) {}
 
@@ -13,17 +15,30 @@ export class PostsService {
     this.http
     .post<{ name: string }>(
       'https://ac-http-default-rtdb.firebaseio.com/posts.json',
-      postData
+      postData,
+      {
+        observe: 'response'
+      }
     )
     .subscribe(responseData => {
       console.log(responseData);
+    }, error => {
+      this.error.next(error.message);
     });
   }
 
   fetchPosts() {
+    let searchParams = new HttpParams();
+    // append search params
+    searchParams = searchParams.append('print', 'pretty');
+    searchParams = searchParams.append('custom', 'key'); // test, not valid 
     return this.http
     .get<{ [key: string]: Post }>(
-      'https://ac-http-default-rtdb.firebaseio.com/posts.json'
+      'https://ac-http-default-rtdb.firebaseio.com/posts.json',
+      {
+        headers: new HttpHeaders({ 'Custom-Header': 'Hello'}),
+        params: searchParams
+      }
     )
     .pipe(
       map(responseData => {
@@ -34,6 +49,10 @@ export class PostsService {
           }
         }
         return postsArray;
+      }),
+      catchError(errorRes => {
+        // Send to analytics servers as an example for using catchError
+        return throwError(errorRes);
       })
     );
   }
@@ -41,4 +60,13 @@ export class PostsService {
   deletePosts() {
     return this.http.delete('https://ac-http-default-rtdb.firebaseio.com/posts.json');
   }
+
+  // getVraAuthToken() {
+  //   return this.http
+  //   .get<{ [key: string]: Post }>('https://ybxd2fcavra0001.onefiserv.net/csp/gateway/am/api/login?access_token'),
+  //   catchError(errorRes => {
+  //     return throwError(errorRes);
+  //   })
+  // }
+
 }
